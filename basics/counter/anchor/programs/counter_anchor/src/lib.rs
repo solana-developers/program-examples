30da42281bb4a4f9e98faa5450dd63e50efa8592
ngundotra <noah@gundotra.org>
2022-09-11 09:59:55 +0530

    counter: add anchor example implementation

---
+++ basics/counter/anchor/programs/counter_anchor/src/lib.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod counter_anchor {
+    use super::*;
+
+    pub fn initialize_counter(ctx: Context<InitializeCounter>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        ctx.accounts.counter.count += 1;
+        Ok(())
+    }
+}
+
+#[account]
+pub struct Counter {
+    count: u64,
+}
+
+#[derive(Accounts)]
+pub struct InitializeCounter<'info> {
+    #[account(init, space=8+8, payer=payer)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
