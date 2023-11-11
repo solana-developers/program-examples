1f32f3e0adfc3dafbbb93985406cc04a69cb02a0
Bram Borggreve <beeman@beeman.nl>
2023-11-11 13:21:58 +0000

    feat: add script to sync versions in package.json files

---
+++ scripts/lib/command-check.ts
@@ -0,0 +1,42 @@
+import { basename } from 'node:path'
+import * as p from 'picocolors'
+import { getDepsCount } from './get-deps-count'
+import { getRecursiveFileList } from './get-recursive-file-list'
+
+export function commandCheck(path: string = '.') {
+  const files = getRecursiveFileList(path).filter((file) => basename(file) === 'package.json')
+  const depsCounter = getDepsCount(files)
+
+  const single: string[] = []
+  const multiple: string[] = []
+
+  Object.keys(depsCounter)
+    .sort()
+    .map((pkg) => {
+      const versions = depsCounter[pkg]
+      const versionMap = Object.keys(versions).sort()
+      const versionsLength = versionMap.length
+
+      if (versionsLength === 1) {
+        const count = versions[versionMap[0]].length
+        single.push(`${p.green(`✔`)} ${pkg}@${versionMap[0]} (${count})`)
+        return
+      }
+
+      const versionCount: { version: string; count: number }[] = []
+      for (const version of versionMap) {
+        versionCount.push({ version, count: versions[version].length })
+      }
+      versionCount.sort((a, b) => b.count - a.count)
+
+      multiple.push(`${p.yellow(`⚠`)} ${pkg} has ${versionsLength} versions:`)
+
+      for (const { count, version } of versionCount) {
+        multiple.push(`  - ${p.bold(version)} (${count})`)
+      }
+    })
+
+  for (const string of [...single.sort(), ...multiple]) {
+    console.log(string)
+  }
+}
---
+++ scripts/lib/get-recursive-file-list.ts
@@ -0,0 +1,28 @@
+// Point method at path and return a list of all the files in the directory recursively
+import { readdirSync, statSync } from 'node:fs'
+
+export function getRecursiveFileList(path: string): string[] {
+  const ignore = ['.git', '.github', '.idea', '.next', '.vercel', '.vscode', 'coverage', 'dist', 'node_modules']
+  const files: string[] = []
+
+  const items = readdirSync(path)
+  items.forEach((item) => {
+    if (ignore.includes(item)) {
+      return
+    }
+    // Check out if it's a directory or a file
+    const isDir = statSync(`${path}/${item}`).isDirectory()
+    if (isDir) {
+      // If it's a directory, recursively call the method
+      files.push(...getRecursiveFileList(`${path}/${item}`))
+    } else {
+      // If it's a file, add it to the array of files
+      files.push(`${path}/${item}`)
+    }
+  })
+
+  return files.filter((file) => {
+    // Remove package.json from the root directory
+    return path === '.' ? file !== './package.json' : true
+  })
+}
