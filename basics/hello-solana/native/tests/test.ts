25e0e978f31444575afd68d5a44c60c91f7fef56
ksolana <110843012+ksolana@users.noreply.github.com>
2024-03-23 23:05:49 -0700

    Fix program keypair file and add assertions to verify program output

--- basics/hello-solana/native/tests/test.ts
+++ basics/hello-solana/native/tests/test.ts
@@ -6,6 +6,8 @@ import {
     TransactionInstruction,
 } from '@solana/web3.js';
 
+import { assert } from "chai";
+
 function createKeypairFromFile(path: string): Keypair {
     return Keypair.fromSecretKey(
         Buffer.from(JSON.parse(require('fs').readFileSync(path, "utf-8")))
@@ -19,8 +21,7 @@ describe("hello-solana", () => {
     //
     const connection = new Connection(`http://localhost:8899`, 'confirmed');
     const payer = createKeypairFromFile(require('os').homedir() + '/.config/solana/id.json');
-    const program = createKeypairFromFile('./program/target/so/program-keypair.json');
-  
+    const program = createKeypairFromFile('./program/target/so/hello_solana_program-keypair.json');
     it("Say hello!", async () => {
 
         // We set up our instruction first.
@@ -35,11 +36,15 @@ describe("hello-solana", () => {
 
         // Now we send the transaction over RPC
         //
-        await sendAndConfirmTransaction(
+        let signature = await sendAndConfirmTransaction(
             connection, 
             new Transaction().add(ix), // Add our instruction (you can add more than one)
             [payer]
         );
+
+        let transaction = await connection.getTransaction(signature, {commitment: "confirmed"});
+        assert(transaction?.meta?.logMessages[0].startsWith("Program " + program.publicKey));
+        assert(transaction?.meta?.logMessages[1] === "Program log: Hello, Solana!");
+        assert(transaction?.meta?.logMessages[2] === "Program log: Our program's Program ID: " + program.publicKey);
     });
   });
-  
