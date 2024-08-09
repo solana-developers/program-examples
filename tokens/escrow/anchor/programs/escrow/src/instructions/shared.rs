b992087cd68ddfe51d69e523585567cb250bc88b
Mike MacCana <mike.maccana@gmail.com>
2024-08-09 13:42:20 -0400

    Use new helper to simplify escrow tests (#108)
    * Use new helper to make Escrow tests faster
    
    * Update pnpm lock
    
    * Add transfer_tokens() to replace copypasted code
    
    * Split up sign_seeds to be less ugly
    
    * Avoid premature optimisation and just move make and take into their respective tests
    
    * Format, no changes
    
    * Make a variable for mocha slow test threshhold
    
    * Make less ugly
    
    * Neaten
    
    * Log signature
    
    * add comments in test to provide context for accounts
    
    ---------
    
    Co-authored-by: John <75003086+ZYJLiu@users.noreply.github.com>

---
+++ tokens/escrow/anchor/programs/escrow/src/instructions/shared.rs
@@ -0,0 +1,25 @@
+use anchor_lang::prelude::*;
+
+use anchor_spl::token_interface::{
+    transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
+};
+
+pub fn transfer_tokens<'info>(
+    from: &InterfaceAccount<'info, TokenAccount>,
+    to: &InterfaceAccount<'info, TokenAccount>,
+    amount: &u64,
+    mint: &InterfaceAccount<'info, Mint>,
+    authority: &Signer<'info>,
+    token_program: &Interface<'info, TokenInterface>,
+) -> Result<()> {
+    let transfer_accounts = TransferChecked {
+        from: from.to_account_info(),
+        mint: mint.to_account_info(),
+        to: to.to_account_info(),
+        authority: authority.to_account_info(),
+    };
+
+    let cpi_context = CpiContext::new(token_program.to_account_info(), transfer_accounts);
+
+    transfer_checked(cpi_context, *amount, mint.decimals)
+}
