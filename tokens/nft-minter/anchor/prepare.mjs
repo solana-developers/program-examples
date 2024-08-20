ed83415bd4993b9257d5a3bb1d19330aae5fa03f
Ayush <ayush-15@live.com>
2024-08-21 00:51:49 +0530

    tokens :construction:

---
+++ tokens/nft-minter/anchor/prepare.mjs
@@ -0,0 +1,31 @@
+#!/usr/bin/env zx
+
+import { $ } from 'zx'
+import { mkdir, rm } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const programs = [{
+  id: 'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+  name: 'token_metadata.so',
+}]
+
+const outputDir = 'tests/fixtures'
+const overwrite = true
+
+try {
+  for (const program of programs) {
+    const { id, name } = program;
+    const outputFile = join(outputDir, name)
+
+    try {
+      await mkdir(outputDir, { recursive: true })
+      if (overwrite) await rm(outputFile, { force: true })
+      await $`solana program dump ${id} ${outputFile}`
+      console.log(`Program ${id} dumped to ${outputFile}`)
+    } catch (error) {
+      console.error(`Error dumping ${id}: ${error.message}`)
+    }
+  }
+} catch (error) {
+  console.error(`Error preparing programs: ${error.message}`)
+}
