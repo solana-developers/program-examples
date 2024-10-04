009a7ba73ebceabae474a6eced31cdbbe90db87f
Ayush <ayush-15@live.com>
2024-10-04 14:21:29 +0530

    add steel framework

---
+++ basics/hello-solana/steel/program/src/lib.rs
@@ -0,0 +1,16 @@
+use steel::*;
+use solana_program::msg;
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("Hello, Solana!");
+
+    msg!("Our program's Program ID: {}", &program_id);
+
+    Ok(())
+}
