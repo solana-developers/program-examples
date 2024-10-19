125b83ce38867eab5970710390b7e56ec373f449
Onyewuchi Emeka <marrnuel123@.com>
2024-10-19 17:25:06 +0100

    chore(): added test coverage using chai and bankrun

--- basics/hello-solana/poseidon/hello-solana/ts-programs/src/hello-solana.ts
+++ basics/hello-solana/poseidon/hello-solana/ts-programs/src/hello-solana.ts
@@ -5,12 +5,9 @@ export default class HelloSolanaProgram {
     "2phbC62wekpw95XuBk4i1KX4uA8zBUWmYbiTMhicSuBV"
   );
 
-  // Define the main 'hello' function similar to the Rust function
   hello(): Result {
-    // Equivalent of Solana's `msg!` for logging
     console.log("Hello, Solana!");
 
-    // Log the program ID as well
     console.log(`Our program's Program ID: ${HelloSolanaProgram.PROGRAM_ID}`);
   }
 }
