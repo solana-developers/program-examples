c8e7f19b3b6620686672e806e8ab843061cd5f01
ved08 <vedvardhan.g@gmail.com>
2024-10-19 18:17:54 +0530

    Add basics/close-account/poseidon

---
+++ basics/close-account/poseidon/programs/close-account/src/lib.rs
@@ -0,0 +1,33 @@
+use anchor_lang::prelude::*;
+declare_id!("11111111111111111111111111111111");
+#[program]
+pub mod close_account {
+    use super::*;
+    pub fn initalize(ctx: Context<InitalizeContext>, data: u8) -> Result<()> {
+        ctx.accounts.state.some_data = data;
+        Ok(())
+    }
+    pub fn close(ctx: Context<CloseContext>) -> Result<()> {
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+pub struct InitalizeContext<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(init, payer = user, space = 9, seeds = [b"account"], bump)]
+    pub state: Account<'info, AccountState>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct CloseContext<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"account"], bump, close = user)]
+    pub state: Account<'info, AccountState>,
+    pub system_program: Program<'info, System>,
+}
+#[account]
+pub struct AccountState {
+    pub some_data: u8,
+}
