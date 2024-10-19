10c112b8d77da9d02f088d95034ce269389b3d3b
Perelyn-sama <okohebina@gmail.com>
2024-10-19 16:20:28 +0100

    add hello solana steel example

---
+++ basics/hello-solana/steel/program/src/hello.rs
@@ -0,0 +1,16 @@
+use solana_sdk::msg;
+use steel::*;
+
+pub fn process_hello(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramResult {
+    // Load accounts.
+    let [signer_info] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+    signer_info.is_signer()?;
+
+    msg!("Hello, Solana!");
+
+    msg!("Our program's Program ID: {}", &steel_api::ID);
+
+    Ok(())
+}
