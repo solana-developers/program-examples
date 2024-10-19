f6212ad114bbdfa6d88c93a7ce6355adee85f05e
Onyewuchi Emeka <marrnuel123@.com>
2024-10-19 16:37:00 +0100

    chore(): added poseidon implementation for counter program

---
+++ basics/counter/poseidon/counter/tests/counter.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Counter } from "../target/types/counter";
+
+describe("counter", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Counter as Program<Counter>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
