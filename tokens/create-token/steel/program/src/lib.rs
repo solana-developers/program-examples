d24b5b00be8df60ac7ed1ad799681713fb29d050
Perelyn-sama <okohebina@gmail.com>
2024-10-22 00:34:01 +0100

    add create token steel example

---
+++ tokens/create-token/steel/program/src/lib.rs
@@ -0,0 +1,22 @@
+mod create;
+
+use create::*;
+
+use steel::*;
+use steel_api::prelude::*;
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    data: &[u8],
+) -> ProgramResult {
+    let (ix, data) = parse_instruction(&steel_api::ID, program_id, data)?;
+
+    match ix {
+        SteelInstruction::Create => process_create(accounts, data)?,
+    }
+
+    Ok(())
+}
+
+entrypoint!(process_instruction);
