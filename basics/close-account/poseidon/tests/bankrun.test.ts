c7d9c9229214e5022de2c2fbad4f8a164867d16d
shivam <shivam.spirite3@outlook.com>
2024-10-22 15:36:35 +0530

    update format

--- basics/close-account/poseidon/tests/bankrun.test.ts
+++ basics/close-account/poseidon/tests/bankrun.test.ts
@@ -1,52 +1,63 @@
-import assert from 'node:assert';
-import { describe, it } from 'node:test';
-import * as anchor from '@coral-xyz/anchor';
-import { PublicKey } from '@solana/web3.js';
-import { BankrunProvider } from 'anchor-bankrun';
-import { startAnchor } from 'solana-bankrun';
-import type { CloseAccountProgram } from '../target/types/close_account_program';
+import assert from "node:assert";
+import { describe, it } from "node:test";
+import * as anchor from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { BankrunProvider } from "anchor-bankrun";
+import { startAnchor } from "solana-bankrun";
+import type { CloseAccountProgram } from "../target/types/close_account_program";
 
-const IDL = require('../target/idl/close_account_program.json');
+const IDL = require("../target/idl/close_account_program.json");
 const PROGRAM_ID = new PublicKey(IDL.address);
 
-describe('close-account', async () => {
-  // Configure the client to use the local cluster.
-  const context = await startAnchor('', [{ name: 'close_account_program', programId: PROGRAM_ID }], []);
-  const provider = new BankrunProvider(context);
+describe("close-account", async () => {
+	// Configure the client to use the local cluster.
+	const context = await startAnchor(
+		"",
+		[{ name: "close_account_program", programId: PROGRAM_ID }],
+		[],
+	);
+	const provider = new BankrunProvider(context);
 
-  const payer = provider.wallet as anchor.Wallet;
-  const program = new anchor.Program<CloseAccountProgram>(IDL, provider);
-  // Derive the PDA for the user's account.
-  const [userAccountAddress] = PublicKey.findProgramAddressSync([Buffer.from('user'), payer.publicKey.toBuffer()], program.programId);
+	const payer = provider.wallet as anchor.Wallet;
+	const program = new anchor.Program<CloseAccountProgram>(IDL, provider);
+	// Derive the PDA for the user's account.
+	const [userAccountAddress] = PublicKey.findProgramAddressSync(
+		[Buffer.from("user"), payer.publicKey.toBuffer()],
+		program.programId,
+	);
 
-  it('Create Account', async () => {
-    await program.methods
-      .createUser()
-      .accounts({
-        user: payer.publicKey,
-      })
-      .rpc();
+	it("Create Account", async () => {
+		await program.methods
+			.createUser()
+			.accounts({
+				user: payer.publicKey,
+			})
+			.rpc();
 
-    // Fetch the account data
-    const userAccount = await program.account.closeAccountState.fetch(userAccountAddress);
-    assert.equal(userAccount.user.toBase58(), payer.publicKey.toBase58());
-  });
+		// Fetch the account data
+		const userAccount =
+			await program.account.closeAccountState.fetch(userAccountAddress);
+		assert.equal(userAccount.user.toBase58(), payer.publicKey.toBase58());
+	});
 
-  it('Close Account', async () => {
-    await program.methods
-      .closeUser()
-      .accounts({
-        user: payer.publicKey,
-      })
-      .rpc();
+	it("Close Account", async () => {
+		await program.methods
+			.closeUser()
+			.accounts({
+				user: payer.publicKey,
+			})
+			.rpc();
 
-    // The account should no longer exist, returning null.
-    try {
-      const userAccount = await program.account.closeAccountState.fetchNullable(userAccountAddress);
-      assert.equal(userAccount, null);
-    } catch (err) {
-      // Won't return null and will throw an error in anchor-bankrun'
-      assert.equal(err.message, `Could not find ${userAccountAddress}`);
-    }
-  });
+		// The account should no longer exist, returning null.
+		try {
+			const userAccount =
+				await program.account.closeAccountState.fetchNullable(
+					userAccountAddress,
+				);
+			assert.equal(userAccount, null);
+		} catch (err) {
+			// Won't return null and will throw an error in anchor-bankrun'
+			assert.equal(err.message, `Could not find ${userAccountAddress}`);
+		}
+	});
 });
--- basics/close-account/poseidon/tests/test.ts
+++ basics/close-account/poseidon/tests/test.ts
@@ -1,49 +1,52 @@
-import assert from 'node:assert';
-import * as anchor from '@coral-xyz/anchor';
-import type { Program } from '@coral-xyz/anchor';
-import { PublicKey } from '@solana/web3.js';
+import assert from "node:assert";
+import * as anchor from "@coral-xyz/anchor";
+import type { Program } from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
 import { CloseAccountProgram } from "../target/types/close_account_program";
 
 describe("close_account", () => {
-  // Configure the client to use the local cluster.
+	// Configure the client to use the local cluster.
 
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
 
-  const program = anchor.workspace.CloseAccountProgram as Program<CloseAccountProgram>;
-  const payer = provider.wallet as anchor.Wallet;
+	const program = anchor.workspace
+		.CloseAccountProgram as Program<CloseAccountProgram>;
+	const payer = provider.wallet as anchor.Wallet;
 
-   // Derive the PDA for the user's account.
-   const [userAccountAddress] = PublicKey.findProgramAddressSync([Buffer.from('user'), payer.publicKey.toBuffer()], program.programId);
+	// Derive the PDA for the user's account.
+	const [userAccountAddress] = PublicKey.findProgramAddressSync(
+		[Buffer.from("user"), payer.publicKey.toBuffer()],
+		program.programId,
+	);
 
+	it("Create Account", async () => {
+		await program.methods
+			.createUser()
+			.accounts({
+				user: payer.publicKey,
+				userAccount: userAccountAddress,
+			})
+			.rpc();
 
-   it('Create Account', async () => {
-    await program.methods
-      .createUser()
-      .accounts({
-        user: payer.publicKey,
-        userAccount: userAccountAddress,
-      })
-      .rpc();
+		// Fetch the account data
+		const userAccount =
+			await program.account.closeAccountState.fetch(userAccountAddress);
+		assert.equal(userAccount.user.toBase58(), payer.publicKey.toBase58());
+	});
 
-    // Fetch the account data
-    const userAccount = await program.account.closeAccountState.fetch(userAccountAddress);
-    assert.equal(userAccount.user.toBase58(), payer.publicKey.toBase58());
-  });
+	it("Close Account", async () => {
+		await program.methods
+			.closeUser()
+			.accounts({
+				user: payer.publicKey,
+				userAccount: userAccountAddress,
+			})
+			.rpc();
 
-  it('Close Account', async () => {
-    await program.methods
-      .closeUser()
-      .accounts({
-        user: payer.publicKey,
-        userAccount: userAccountAddress,
-      })
-      .rpc();
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.closeAccountState.fetchNullable(userAccountAddress);
-    assert.equal(userAccount, null);
-  });
-
- 
+		// The account should no longer exist, returning null.
+		const userAccount =
+			await program.account.closeAccountState.fetchNullable(userAccountAddress);
+		assert.equal(userAccount, null);
+	});
 });
