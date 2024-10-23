0da12a7fc42d0617da4bc1206a18157e211f2ab7
adpthegreat <adpthegreatanddefiant@gmail.com>
2024-10-23 16:11:35 +0100

    update poseidon_rent_payer_code

--- basics/pda-rent-payer/poseidon/pda_rent_payer/programs/pda_rent_payer/src/lib.rs
+++ basics/pda-rent-payer/poseidon/pda_rent_payer/programs/pda_rent_payer/src/lib.rs
@@ -1,40 +1,52 @@
 use anchor_lang::prelude::*;
-use anchor_lang::system_program::{transfer, Transfer};
+use anchor_lang::system_program::{Transfer, transfer};
 declare_id!("BYj8GpV9hpv9PAVdwoWFCTMkysJkk5jstYjuCrw4pxem");
 #[program]
 pub mod pda_rent_payer {
     use super::*;
-    pub fn init_rent_vault(
-        ctx: Context<InitRentVaultContext>,
-        fund_lamports: u64,
+    pub fn init_rent_vault(ctx: Context<InitRentVaultContext>) -> Result<()> {
+        ctx.accounts.state.owner = ctx.accounts.owner.key();
+        ctx.accounts.state.state_bump = ctx.bumps.state;
+        ctx.accounts.state.auth_bump = ctx.bumps.auth;
+        ctx.accounts.state.vault_bump = ctx.bumps.vault;
+        Ok(())
+    }
+    pub fn deposit_to_rent_vault(
+        ctx: Context<DepositToRentVaultContext>,
+        amount: u64,
     ) -> Result<()> {
-        ctx.accounts.rent_vault.rent_vault_bump = ctx.bumps.rent_vault;
         let transfer_accounts = Transfer {
             from: ctx.accounts.owner.to_account_info(),
-            to: ctx.accounts.rent_vault.to_account_info(),
+            to: ctx.accounts.vault.to_account_info(),
         };
         let cpi_ctx = CpiContext::new(
             ctx.accounts.system_program.to_account_info(),
             transfer_accounts,
         );
-        transfer(cpi_ctx, fund_lamports)?;
+        transfer(cpi_ctx, amount)?;
         Ok(())
     }
     pub fn create_new_account(
         ctx: Context<CreateNewAccountContext>,
-        transfer_amount: u64,
+        amount: u64,
     ) -> Result<()> {
-        ctx.accounts.new_account.owner = ctx.accounts.owner.key();
-        ctx.accounts.new_account.account_bump = ctx.bumps.new_account;
+        ctx.accounts.new_account_state.owner = ctx.accounts.owner.key();
         let transfer_accounts = Transfer {
-            from: ctx.accounts.rent_vault.to_account_info(),
-            to: ctx.accounts.new_account.to_account_info(),
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.new_account_state.to_account_info(),
         };
-        let cpi_ctx = CpiContext::new(
+        let seeds = &[
+            b"vault",
+            ctx.accounts.auth.to_account_info().key.as_ref(),
+            &[ctx.accounts.state.vault_bump],
+        ];
+        let pda_signer = &[&seeds[..]];
+        let cpi_ctx = CpiContext::new_with_signer(
             ctx.accounts.system_program.to_account_info(),
             transfer_accounts,
+            pda_signer,
         );
-        transfer(cpi_ctx, transfer_amount)?;
+        transfer(cpi_ctx, amount)?;
         Ok(())
     }
 }
@@ -43,39 +55,74 @@ pub struct InitRentVaultContext<'info> {
     #[account(
         init,
         payer = owner,
-        space = 9,
-        seeds = [b"rent_vault",
+        space = 8,
+        seeds = [b"vault",
+        auth.key().as_ref()],
+        bump,
+    )]
+    pub vault: Account<'info, RentVault>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(
+        init,
+        payer = owner,
+        space = 43,
+        seeds = [b"state",
         owner.key().as_ref()],
         bump,
     )]
-    pub rent_vault: Account<'info, RentState>,
+    pub state: Account<'info, RentAccountState>,
+    #[account(seeds = [b"auth", state.key().as_ref()], bump)]
+    /// CHECK: This acc is safe
+    pub auth: UncheckedAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct DepositToRentVaultContext<'info> {
     #[account(mut)]
     pub owner: Signer<'info>,
+    #[account(seeds = [b"auth", state.key().as_ref()], bump = state.auth_bump)]
+    /// CHECK: This acc is safe
+    pub auth: UncheckedAccount<'info>,
+    #[account(seeds = [b"vault", auth.key().as_ref()], bump = state.vault_bump)]
+    pub vault: Account<'info, RentVault>,
+    #[account(seeds = [b"state", owner.key().as_ref()], bump = state.state_bump)]
+    pub state: Account<'info, RentAccountState>,
     pub system_program: Program<'info, System>,
 }
 #[derive(Accounts)]
 pub struct CreateNewAccountContext<'info> {
-    #[account()]
-    pub rent_vault: Account<'info, RentState>,
     #[account(
         init,
         payer = owner,
         space = 41,
-        seeds = [b"account",
+        seeds = [b"new_account",
         owner.key().as_ref()],
         bump,
     )]
-    pub new_account: Account<'info, AccountState>,
+    pub new_account_state: Account<'info, NewAccountState>,
     #[account(mut)]
     pub owner: Signer<'info>,
+    #[account(seeds = [b"auth", state.key().as_ref()], bump = state.auth_bump)]
+    /// CHECK: This acc is safe
+    pub auth: UncheckedAccount<'info>,
+    #[account(mut, seeds = [b"vault", auth.key().as_ref()], bump = state.vault_bump)]
+    pub vault: SystemAccount<'info>,
+    #[account(seeds = [b"state", owner.key().as_ref()], bump = state.state_bump)]
+    pub state: Account<'info, RentAccountState>,
     pub system_program: Program<'info, System>,
 }
 #[account]
-pub struct AccountState {
+pub struct RentAccountState {
     pub owner: Pubkey,
-    pub account_bump: u8,
+    pub state_bump: u8,
+    pub auth_bump: u8,
+    pub vault_bump: u8,
 }
 #[account]
-pub struct RentState {
-    pub rent_vault_bump: u8,
+pub struct NewAccountState {
+    pub owner: Pubkey,
+    pub new_account_bump: u8,
 }
+#[account]
+pub struct RentVault {}
--- basics/pda-rent-payer/poseidon/pda_rent_payer/ts-programs/src/pda_rent_payer.ts
+++ basics/pda-rent-payer/poseidon/pda_rent_payer/ts-programs/src/pda_rent_payer.ts
@@ -1,40 +1,101 @@
-import { Account, Mint, Pubkey, Result, Signer, SystemAccount, SystemProgram, TokenAccount, u8, u64 } from '@solanaturbine/poseidon';
+import {
+  Account,
+  Mint,
+  Pubkey,
+  Result,
+  Signer,
+  SystemAccount,
+  SystemProgram,
+  u8,
+  u64,
+  UncheckedAccount,
+} from "@solanaturbine/poseidon";
 
 export default class PdaRentPayer {
-  static PROGRAM_ID = new Pubkey('BYj8GpV9hpv9PAVdwoWFCTMkysJkk5jstYjuCrw4pxem');
+  static PROGRAM_ID = new Pubkey(
+    "BYj8GpV9hpv9PAVdwoWFCTMkysJkk5jstYjuCrw4pxem"
+  );
 
   // When lamports are transferred to a new address (without and existing account),
   // An account owned by the system program is created by default
-  initRentVault(owner: Signer, rent_vault: RentState, fund_lamports: u64) {
-    //Derive and intialize the vault account with bump and seeds
-    rent_vault.derive(['rent_vault', owner.key]).init();
+  initRentVault(
+    owner: Signer,
+    vault: RentVault,
+    state: RentAccountState,
+    auth: UncheckedAccount,
+  ) {
+    //Derive the accounts with bump and seeds
+    vault.derive(["vault", auth.key]).init();
+    state.derive(["state", owner.key]).init()
+    auth.derive(["auth", state.key]);
 
-    rent_vault.rentVaultBump = rent_vault.getBump();
-    // Transfer specified lamports from payer to the rent vault to initialize the rent vault
-    SystemProgram.transfer(owner, rent_vault, fund_lamports, ['rent_vault', owner.key, rent_vault.rentVaultBump]);
+    state.owner = owner.key;
+
+    //To store bumps in the RentAccountState, we simply call getBump on the RentAccountState
+    state.stateBump = state.getBump();
+    state.authBump = auth.getBump();
+    state.vaultBump = vault.getBump();
   }
 
-  // Create a new system account
-  createNewAccount(owner: Signer, rent_vault: RentState, new_account: AccountState, transfer_amount: u64): Result {
-    //We use derive to define the account and chain the `.init()` at the end for creating the account
-    new_account.derive(['account', owner.key]).init();
 
-    //Set owner of the account
-    new_account.owner = owner.key;
+  depositToRentVault(
+    owner: Signer,
+    state: RentAccountState,
+    auth: UncheckedAccount,
+    vault: RentVault,
+    amount: u64
+  ) {
+    //Since we have stored bumps in the RentAccountState we can derive PDAs with stored bumps by passing that as the second argument
+    state.deriveWithBump(["state", owner.key], state.stateBump);
+    auth.deriveWithBump(["auth", state.key], state.authBump);
+    vault.deriveWithBump(["vault", auth.key], state.vaultBump);
 
-    // Store bump for the account
-    new_account.accountBump = new_account.getBump();
+    // Transfer specified lamports from owner to the rent vault
+    SystemProgram.transfer(
+      owner, //from
+      vault, //to
+      amount //amount to be sent
+    );
+  }
+
+  //Deposit some sol into out PDA to pay for rent
+  createNewAccount(
+    owner: Signer,
+    state: RentAccountState,
+    auth: UncheckedAccount,
+    vault: SystemAccount,
+    new_account_state: NewAccountState,
+    amount: u64
+  ): Result {
+    new_account_state.derive(["new_account", owner.key]).init();
+
+    state.deriveWithBump(["state", owner.key], state.stateBump);
+    auth.deriveWithBump(["auth", state.key], state.authBump);
+    vault.deriveWithBump(["vault", auth.key], state.vaultBump);
+
+    // state.newAccountBump = new_account.getBump(); // we don't need the new_account bump
+
+    new_account_state.owner = owner.key;
 
     // We now transfer the lamports from the rent_vault to the new account
-    SystemProgram.transfer(rent_vault, new_account, transfer_amount, ['rent_vault', owner.key, rent_vault.rentVaultBump]);
+    SystemProgram.transfer(vault, new_account_state, amount, [
+      "vault",
+      auth.key,
+    ]);
   }
 }
 
-export interface AccountState extends Account {
+export interface RentAccountState extends Account {
   owner: Pubkey; // Owner of the account
-  accountBump: u8; // Bump for the derived account
+  stateBump: u8; // Bump for the state account
+  authBump: u8; // Bump for the auth account
+  vaultBump: u8; // Bump for the vault account
 }
 
-export interface RentState extends Account {
-  rentVaultBump: u8; // Bump for rent Vault Pda
+//Our vault here is CustomStateAccount not a SystemAccount
+export interface RentVault extends Account {}
+
+export interface NewAccountState extends Account {
+    owner:Pubkey,
+  newAccountBump: u8; // Bump for the newly created account
 }
