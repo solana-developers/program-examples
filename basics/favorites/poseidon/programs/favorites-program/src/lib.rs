2aade59c7b9c3da57ee9cf8abab7925e738e10b2
Mwaa Joseph <joseph@kamoa.app>
2024-10-23 08:28:58 +0300

    Add the files for basics/favorites/poseidon

---
+++ basics/favorites/poseidon/programs/favorites-program/src/lib.rs
@@ -0,0 +1,40 @@
+use anchor_lang::prelude::*;
+declare_id!("HMYL9ABJz8fpw6XUnkRAYVsXor4JxosiZqHBd38ZgCqS");
+#[program]
+pub mod favorites_program {
+    use super::*;
+    pub fn set_favorites(
+        ctx: Context<SetFavoritesContext>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        ctx.accounts.favorites.number = number;
+        ctx.accounts.favorites.color = color;
+        ctx.accounts.favorites.hobbies = hobbies;
+        ctx.accounts.favorites.bump = ctx.bumps.favorites;
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+pub struct SetFavoritesContext<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        init,
+        payer = payer,
+        space = 217,
+        seeds = [b"favorites",
+        payer.key().as_ref()],
+        bump,
+    )]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
+}
+#[account]
+pub struct Favorites {
+    pub number: u64,
+    pub color: String,
+    pub hobbies: Vec<String>,
+    pub bump: u8,
+}
