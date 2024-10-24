d186dd39a5805a5af7b9ac7048e1a3c61ce0f3cb
shivam <shivam.spirite3@outlook.com>
2024-10-24 21:17:14 +0530

    update format

--- basics/processing-instructions/poseidon/ts-programs/src/processingInstructionsProgram.ts
+++ basics/processing-instructions/poseidon/ts-programs/src/processingInstructionsProgram.ts
@@ -1,18 +1,15 @@
-import { Pubkey, Result, u32 } from "@solanaturbine/poseidon";
+import { Pubkey, Result, u32 } from '@solanaturbine/poseidon';
 
 export default class ProcessingInstructionsProgram {
-  static PROGRAM_ID = new Pubkey("6Ra6xuBTuL8ytoHsGEzaAQcHHqNyGjjzemHpFyYrpGdV");
+  static PROGRAM_ID = new Pubkey('6Ra6xuBTuL8ytoHsGEzaAQcHHqNyGjjzemHpFyYrpGdV');
 
   // for processing instruction in poseidon we need to just write a method that will transpile into anchor instruction
 
-  processingInstructions(height:u32): Result {
-
+  processingInstructions(height: u32): Result {
     if (Number(height) > 8) {
-        console.log(`You can jump from this height length`);
+      console.log('You can jump from this height length');
     } else {
-       console.log(`you can not jump because you are too short to handle height`);
-    };
-
+      console.log('you can not jump because you are too short to handle height');
+    }
   }
-
 }
