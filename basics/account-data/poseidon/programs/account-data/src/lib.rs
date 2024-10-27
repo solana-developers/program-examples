05ffade6662928ae38d3748fb0b38bda9e891e11
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2024-10-27 23:39:56 +0530

    fix

--- basics/account-data/poseidon/programs/account-data/src/lib.rs
+++ basics/account-data/poseidon/programs/account-data/src/lib.rs
@@ -5,10 +5,15 @@ pub mod account_data {
     use super::*;
     pub fn create_address_info(
         ctx: Context<CreateAddressInfoContext>,
-        name: Vec<u8>,
+        name: String,
         house_number: u8,
+        street: String,
+        city: String,
     ) -> Result<()> {
+        ctx.accounts.address_info.name = name;
         ctx.accounts.address_info.house_number = house_number;
+        ctx.accounts.address_info.street = street;
+        ctx.accounts.address_info.city = city;
         Ok(())
     }
 }
@@ -20,7 +25,7 @@ pub struct CreateAddressInfoContext<'info> {
 }
 #[account]
 pub struct AddressInfo {
-    pub name: Uint8Array,
+    pub name: String,
     pub house_number: u8,
     pub street: String,
     pub city: String,
