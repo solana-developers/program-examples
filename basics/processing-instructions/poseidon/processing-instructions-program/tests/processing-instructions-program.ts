82277ef0e2049bace24b276b948d959ffe8ca789
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-10-28 18:22:03 +0530

    added basics/processing-instructions/poseidon

---
+++ basics/processing-instructions/poseidon/processing-instructions-program/tests/processing-instructions-program.ts
@@ -0,0 +1,36 @@
+// import * as anchor from '@coral-xyz/anchor';
+// import { Program } from '@coral-xyz/anchor';
+// import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
+// import { assert } from 'chai';
+// import { ProcessingInstructionsProgram } from '../target/types/processing_instructions_program';
+
+// describe('processing_instructions_program', () => {
+//   // Configure the client to use the local cluster.
+//   const provider = anchor.AnchorProvider.env();
+//   anchor.setProvider(provider);
+//   const program = anchor.workspace.ProcessingInstructionsProgram as Program<ProcessingInstructionsProgram>;
+//   const payer = provider.wallet as anchor.Wallet;
+
+//   it('Tests the go_to_park function', async () => {
+
+//     // Define the test parameters
+//     const height = 6;
+//     const name = "Alice";
+
+//     // Call the go_to_park function in the Solana program
+//     const tx = await program.methods
+//       .goToPark(height, name)
+//       .accounts({
+//         user: payer.publicKey,
+//       })
+//       .signers([payer.payer])
+//       .rpc();
+
+//     console.log('Your transaction signature', tx);
+
+//     // Assertions can be made here based on expected behavior
+//     // Since we are using msg! for console messages, we don't have a direct way to capture these outputs in tests
+//     // However, we can still verify the transaction was successful
+//     assert.isNotNull(tx, 'Transaction should be successful');
+//   });
+// });
---
+++ basics/processing-instructions/poseidon/processing-instructions-program/ts-programs/src/processingInstructionsProgram.ts
@@ -0,0 +1,16 @@
+import { Account, Pubkey, Result, u32 } from '@solanaturbine/poseidon';
+
+export default class ProcessingInstructionsProgram {
+  static PROGRAM_ID = new Pubkey('3tmL8d7yC7cRDwQ1B8rcsY6YzWTGVz65aw1ooH2YbnME');
+
+  go_to_park(height: u32, name: string): Result {
+    // Display a welcome message
+    console.log('Welcome to the park,', name);
+    // Check if the height is above the threshold
+    if (Number(height) > 5) {
+      console.log('You are tall enough to ride this ride. Congratulations.');
+    } else {
+      console.log('You are NOT tall enough to ride this ride. Sorry mate.');
+    }
+  }
+}
