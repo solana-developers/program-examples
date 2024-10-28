c75d76c4300e9a7ecf536973b69ea3b952fdf7a2
leopham <hongthaipro@gmail.com>
2024-10-28 15:09:28 +0000

    Done token escrow with steel

---
+++ tokens/escrow/steel/api/src/consts.rs
@@ -0,0 +1,8 @@
+use solana_program::pubkey;
+use steel::Pubkey;
+
+/// Seed of the offer account PDA.
+pub const OFFER_SEED: &[u8] = b"offer";
+
+pub const ASSOCIATED_TOKEN_PROGRAM_ID: Pubkey =
+    pubkey!("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
