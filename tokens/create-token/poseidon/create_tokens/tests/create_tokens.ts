f2b20c3ee56bb0390c610fb0e3afc40321571fa0
adpthegreat <adpthegreatanddefiant@gmail.com>
2024-10-29 02:45:08 +0100

    feat:ran pnpm fix

--- tokens/create-token/poseidon/create_tokens/tests/create_tokens.ts
+++ tokens/create-token/poseidon/create_tokens/tests/create_tokens.ts
@@ -1,16 +1,16 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { CreateTokens } from "../target/types/create_tokens";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { CreateTokens } from '../target/types/create_tokens';
 
-describe("create_tokens", () => {
+describe('create_tokens', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.CreateTokens as Program<CreateTokens>;
 
-  it("Is initialized!", async () => {
+  it('Is initialized!', async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log('Your transaction signature', tx);
   });
 });
