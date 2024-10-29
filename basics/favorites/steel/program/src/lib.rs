2394aca1e144b7de737a344b930fa814c3416a38
chizor iwuh <thisischizor@gmail.com>
2024-10-29 08:02:01 +0100

    favorites/steel

---
+++ basics/favorites/steel/program/src/lib.rs
@@ -0,0 +1,69 @@
+use std::ffi::CStr;
+
+use solana_program::msg;
+use steel::*;
+
+declare_id!("z7msBPQHDJjTvdQRoEcKyENgXDhSRYeHieN1ZMTqo35");
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    data: &[u8],
+) -> ProgramResult {
+    let favorites_data = bytemuck::try_from_bytes::<Favorites>(data)
+        .or(Err(ProgramError::InvalidInstructionData))?;
+
+    let [favorites_info, user, system_program] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    favorites_info.is_writable()?;
+
+    // if we have not created our favourites account, let us create it
+    if favorites_info.lamports() == 0 {
+        create_account::<Favorites>(
+            favorites_info,
+            system_program,
+            user,
+            program_id,
+            &[b"favorites", user.key.as_ref()],
+        )?;
+    }
+
+    let favorites = favorites_info.as_account_mut::<Favorites>(program_id)?;
+
+    *favorites = *favorites_data;
+
+    let favorite_number = favorites.number;
+
+    msg!("Favorite number: {}", favorite_number);
+    msg!(
+        "Favorite color: {:?}",
+        CStr::from_bytes_until_nul(&favorites.color).unwrap()
+    );
+    for i in 0..favorites.hobbies.len() {
+        msg!(
+            "Favorite hobby {i}: {:?}",
+            CStr::from_bytes_until_nul(&favorites.hobbies[i]).unwrap()
+        );
+    }
+
+    Ok(())
+}
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, IntoPrimitive, TryFromPrimitive)]
+pub enum SteelAccount {
+    Favorites = 0,
+}
+
+account!(SteelAccount, Favorites);
+#[repr(C, packed)]
+#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
+pub struct Favorites {
+    pub number: u64,
+    pub color: [u8; 48],
+    pub hobbies: [[u8; 48]; 5],
+}
---
+++ basics/favorites/steel/tests/test.ts
@@ -0,0 +1,125 @@
+import { Buffer } from 'node:buffer';
+import { describe, test } from 'node:test';
+import { Keypair, PublicKey, SystemProgram, Transaction, TransactionInstruction } from '@solana/web3.js';
+import * as borsh from 'borsh';
+import { start } from 'solana-bankrun';
+
+class Favorites {
+  number: bigint;
+  color: Uint8Array;
+  hobbies: Uint8Array[];
+
+  constructor(props: {
+    number: bigint;
+    color: Uint8Array;
+    hobbies: Uint8Array[];
+  }) {
+    this.number = props.number;
+    this.color = props.color;
+    this.hobbies = props.hobbies;
+  }
+
+  toBuffer() {
+    return Buffer.from(borsh.serialize(FavoritesSchema, this));
+  }
+
+  static fromAccountData(buffer: Buffer) {
+    const _accountData = Uint8Array.from(buffer).slice(8); // remove 8 byte discriminator
+
+    return borsh.deserialize(FavoritesSchema, Favorites, Buffer.from(_accountData));
+  }
+
+  static fromBuffer(buffer: Buffer) {
+    const some = Uint8Array.from(buffer).slice(8);
+
+    return borsh.deserialize(FavoritesSchema, Favorites, Buffer.from(some));
+  }
+
+  static fromInfo(info: { number: number; color: string; hobbies: string[] }) {
+    return new Favorites({
+      number: BigInt(info.number),
+      color: Uint8Array.from(Buffer.from(info.color.padEnd(48, '\0'))),
+      hobbies: info.hobbies.map((hobby) => Uint8Array.from(Buffer.from(hobby.padEnd(48, '\0')))),
+    });
+  }
+
+  serialise() {
+    return {
+      number: this.number.toString(),
+      color: Buffer.from(this.color).toString(),
+      hobbies: this.hobbies.map((hobby) => Buffer.from(hobby).toString()),
+    };
+  }
+}
+
+const FavoritesSchema = new Map([
+  [
+    Favorites,
+    {
+      kind: 'struct',
+      fields: [
+        ['number', 'u64'],
+        ['color', [48]], // Fixed array of 48 bytes
+        ['hobbies', [[48], 5]], // Fixed array of 5 48 bytes array
+      ],
+    },
+  ],
+]);
+
+describe('Favorites!', async () => {
+  const addressInfoAccount = Keypair.generate();
+  const PROGRAM_ID = PublicKey.unique();
+  const context = await start([{ name: 'favorites_steel_program', programId: PROGRAM_ID }], []);
+  const client = context.banksClient;
+
+  function favoritesPda(userPubkey: PublicKey) {
+    return PublicKey.findProgramAddressSync([Buffer.from('favorites'), userPubkey.toBuffer()], PROGRAM_ID);
+  }
+
+  test('Set the favorites data', async () => {
+    const payer = context.payer;
+
+    console.log(`Program Address    : ${PROGRAM_ID}`);
+    console.log(`Payer Address      : ${payer.publicKey}`);
+    console.log(`Address Info Acct  : ${addressInfoAccount.publicKey}`);
+
+    const ix = new TransactionInstruction({
+      keys: [
+        {
+          pubkey: favoritesPda(payer.publicKey)[0],
+          isSigner: false,
+          isWritable: true,
+        },
+        {
+          pubkey: payer.publicKey,
+          isSigner: true,
+          isWritable: true,
+        },
+        { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
+      ],
+      programId: PROGRAM_ID,
+      data: Favorites.fromInfo({
+        number: 2,
+        color: 'green',
+        hobbies: ['singing', 'reading', 'jogging', 'dancing', 'traveling'],
+      }).toBuffer(),
+    });
+
+    const blockhash = context.lastBlockhash;
+
+    const tx = new Transaction();
+    tx.recentBlockhash = blockhash;
+    tx.add(ix).sign(payer);
+    await client.processTransaction(tx);
+  });
+
+  test("Read favorites's data", async () => {
+    const accountInfo = await client.getAccount(favoritesPda(context.payer.publicKey)[0]);
+
+    const readFavorites = Favorites.fromAccountData(Buffer.from(accountInfo.data)).serialise();
+
+    console.log(`number : ${readFavorites.number}`);
+    console.log(`color  : ${readFavorites.color}`);
+    console.log(`Hobbies: ${readFavorites.hobbies}`);
+  });
+});
