fed2cb70d0ce1dddd154de28b99b0e3140298deb
aryan <agarwalaryan139@gmail.com>
2024-11-06 14:58:26 +0530

    chore: linting

--- basics/counter/steel/tests/test.ts
+++ basics/counter/steel/tests/test.ts
@@ -1,38 +1,27 @@
-import { describe, it } from "mocha";
-import {
-  PublicKey,
-  Transaction,
-  TransactionInstruction,
-  SystemProgram,
-} from "@solana/web3.js";
-import { assert } from "chai";
-import { start } from "solana-bankrun";
+import { PublicKey, SystemProgram, Transaction, TransactionInstruction } from '@solana/web3.js';
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+import { start } from 'solana-bankrun';
 
-describe("hello-solana", function () {
+describe('hello-solana', () => {
   let context: any;
   let client: any;
   let payer: any;
   const PROGRAM_ID = PublicKey.unique();
 
-  const COUNTER_SEED = Buffer.from("counter");
+  const COUNTER_SEED = Buffer.from('counter');
 
-  before(async function () {
+  before(async () => {
     // load program in solana-bankrun
-    context = await start(
-      [{ name: "steel_program", programId: PROGRAM_ID }],
-      []
-    );
+    context = await start([{ name: 'steel_program', programId: PROGRAM_ID }], []);
     client = context.banksClient;
     payer = context.payer;
   });
 
-  it("Say hello!", async function () {
+  it('Say hello!', async () => {
     const blockhash = context.lastBlockhash;
 
-    const [counterPDA, bump] = PublicKey.findProgramAddressSync(
-      [COUNTER_SEED],
-      PROGRAM_ID
-    );
+    const [counterPDA, bump] = PublicKey.findProgramAddressSync([COUNTER_SEED], PROGRAM_ID);
 
     // We set up our instruction first.
     const ix = new TransactionInstruction({
@@ -52,16 +41,13 @@ describe("hello-solana", function () {
     // Now we process the transaction
     const transaction = await client.processTransaction(tx);
 
-    assert(transaction.logMessages[3].startsWith(`Program log: Initialized`));
+    assert(transaction.logMessages[3].startsWith('Program log: Initialized'));
   });
 
-  it("Increment", async function () {
+  it('Increment', async () => {
     const blockhash = context.lastBlockhash;
 
-    const [counterPDA, bump] = PublicKey.findProgramAddressSync(
-      [COUNTER_SEED],
-      PROGRAM_ID
-    );
+    const [counterPDA, bump] = PublicKey.findProgramAddressSync([COUNTER_SEED], PROGRAM_ID);
 
     // We set up our instruction first.
     const ix = new TransactionInstruction({
@@ -80,8 +66,8 @@ describe("hello-solana", function () {
     // Now we process the transaction
     const transaction = await client.processTransaction(tx);
 
-    assert(transaction.logMessages[1] === `Program log: Increment`);
-    assert(transaction.logMessages[2] === `Program log: Counter Value 1`);
+    assert(transaction.logMessages[1] === 'Program log: Increment');
+    assert(transaction.logMessages[2] === 'Program log: Counter Value 1');
     assert(transaction.logMessages.length === 5);
   });
 });
