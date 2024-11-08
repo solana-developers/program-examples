8305a3bc84aecff8d1d9b8f6cf0f948d274893bc
chizor iwuh <thisischizor@gmail.com>
2024-11-08 15:55:51 +0100

    realloc/steel - add api

---
+++ basics/realloc/steel/api/src/lib.rs
@@ -0,0 +1,11 @@
+mod instruction;
+mod state;
+
+use steel::*;
+
+declare_id!("z7msBPQHDJjTvdQRoEcKyENgXDhSRYeHieN1ZMTqo35");
+
+pub mod prelude {
+    pub use crate::instruction::*;
+    pub use crate::state::*;
+}
