d7b7730e0ebd830deee68be58384e44b2f93bad9
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2024-11-18 19:42:44 +0530

    fix: lint

--- basics/program-derived-addresses/poseidon/ts-programs/src/pda.ts
+++ basics/program-derived-addresses/poseidon/ts-programs/src/pda.ts
@@ -1,30 +1,21 @@
-import {
-	Account,
-	Pubkey,
-	Result,
-	Signer,
-	u32,
-	u8,
-} from "@solanaturbine/poseidon";
+import { Account, Pubkey, Result, Signer, u8, u32 } from '@solanaturbine/poseidon';
 
 export default class ProgramDerivedAddresses {
-	static PROGRAM_ID = new Pubkey(
-		"HcZYDcJ4AF3LgKnDaF6jKyBY7Lf2zggPn4vdGvHBACiW"
-	);
+  static PROGRAM_ID = new Pubkey('HcZYDcJ4AF3LgKnDaF6jKyBY7Lf2zggPn4vdGvHBACiW');
 
-	createPageVisits(pageVisits: PageVisits, user: Signer): Result {
-		pageVisits.derive(["page_visits", user.key]).init();
+  createPageVisits(pageVisits: PageVisits, user: Signer): Result {
+    pageVisits.derive(['page_visits', user.key]).init();
 
-		pageVisits.pageVisits = new u32(0);
-		pageVisits.bump = pageVisits.getBump();
-	}
+    pageVisits.pageVisits = new u32(0);
+    pageVisits.bump = pageVisits.getBump();
+  }
 
-	incrementPageVisits(pageVisits: PageVisits): Result {
-		pageVisits.pageVisits = pageVisits.pageVisits.add(1);
-	}
+  incrementPageVisits(pageVisits: PageVisits): Result {
+    pageVisits.pageVisits = pageVisits.pageVisits.add(1);
+  }
 }
 
 export interface PageVisits extends Account {
-	pageVisits: u32;
-	bump: u8;
+  pageVisits: u32;
+  bump: u8;
 }
