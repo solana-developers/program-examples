66891257f14ce8d2e63ace4b20e06add9fa10e05
Ayush <ayush-15@live.com>
2024-11-23 00:20:27 +0530

    poseidon hello-solana

---
+++ basics/hello-solana/poseidon/ts-programs/src/poseidon.ts
@@ -0,0 +1,9 @@
+import { Pubkey, type Result } from '@solanaturbine/poseidon';
+
+export default class Poseidon {
+  static PROGRAM_ID = new Pubkey('8krac1be77qraaZQGzHypmngXH6MbftjBmcucLtQLahG');
+
+  initialize(): Result {
+    // Write your program here
+  }
+}
