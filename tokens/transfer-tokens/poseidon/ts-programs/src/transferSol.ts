7e60c06e18083debc297a35e603fddce2266dd6c
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-11-22 19:13:59 +0100

    Remove extra part

--- tokens/transfer-tokens/poseidon/ts-programs/src/transferSol.ts
+++
@@ -1,9 +0,0 @@
-import { Pubkey, Result, Signer, SystemAccount, SystemProgram, u64 } from '@solanaturbine/poseidon';
-
-export default class TransferSol {
-  static PROGRAM_ID = new Pubkey('HU7QEokj5qUUV5ryZYL7EhsqiAkxJyMVXc3DesKyCqtF');
-
-  transferWithProgram(from: Signer, to: SystemAccount, amount: u64): Result {
-    SystemProgram.transfer(from, to, amount);
-  }
-}
