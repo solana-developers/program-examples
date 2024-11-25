61fe4f03471e0c0c3ff77e5fa3b26e0dd791202b
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-11-25 08:31:58 +0100

    Formatting

--- tokens/transfer-tokens/poseidon/tests/transfer_sol.ts
+++ tokens/transfer-tokens/poseidon/tests/transfer_sol.ts
@@ -1,9 +1,9 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { BN } from "bn.js";
-import { TransferSol } from "../target/types/transfer_sol";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { BN } from 'bn.js';
+import { TransferSol } from '../target/types/transfer_sol';
 
-describe("transfer_sol", () => {
+describe('transfer_sol', () => {
   //UTILS
   const confirm = async (signature: string): Promise<string> => {
     const block = await provider.connection.getLatestBlockhash();
@@ -28,14 +28,12 @@ describe("transfer_sol", () => {
   const airdropAmount = 2.5 * anchor.web3.LAMPORTS_PER_SOL;
   const transferAmount = 1 * anchor.web3.LAMPORTS_PER_SOL;
 
-  it("Airdrop", async () => {
-    const airdropMaker = await provider.connection
-      .requestAirdrop(maker.publicKey, airdropAmount)
-      .then(confirm);
-    console.log("\nAirdropped 5 SOL to maker", airdropMaker);
+  it('Airdrop', async () => {
+    const airdropMaker = await provider.connection.requestAirdrop(maker.publicKey, airdropAmount).then(confirm);
+    console.log('\nAirdropped 5 SOL to maker', airdropMaker);
   });
 
-  it("Transfer token", async () => {
+  it('Transfer token', async () => {
     // Add your test here.
     const tx = await program.methods
       .transferWithProgram(new BN(transferAmount))
@@ -45,6 +43,6 @@ describe("transfer_sol", () => {
       })
       .signers([maker])
       .rpc();
-    console.log("Your transaction signature", tx);
+    console.log('Your transaction signature', tx);
   });
 });
