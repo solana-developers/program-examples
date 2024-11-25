2bec8b279e8f6031f12d8f1995630aae24b11ba3
Ritik Bhatt <120605862+ritikbhatt20@users.noreply.github.com>
2024-11-25 19:34:03 +0530

    made fix in processingInstructionsProgram.ts

--- basics/processing-instructions/poseidon/processing-instructions-program/ts-programs/src/processingInstructionsProgram.ts
+++ basics/processing-instructions/poseidon/processing-instructions-program/ts-programs/src/processingInstructionsProgram.ts
@@ -3,7 +3,7 @@ import { Account, Pubkey, type Result, u32 } from '@solanaturbine/poseidon';
 export default class ProcessingInstructionsProgram {
   static PROGRAM_ID = new Pubkey('FUfFBrs2nHAud8gVESDMtYa7oa5aGa3DEngKKLGyV2hv');
 
-  go_to_park(height: u32, name: string): Result {
+  go_to_park(height: u32, name: String): Result {
     // Display a welcome message
     console.log('Welcome to the park,', name);
     // Check if the height is above the threshold
