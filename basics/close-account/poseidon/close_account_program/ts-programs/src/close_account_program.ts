42b1f9b052819cd560f88726dbe10f65c24f39c7
adpthegreat <adpthegreatanddefiant@gmail.com>
2024-11-25 09:28:17 +0100

    feat:fixed strings, updated cargo.lock

--- basics/close-account/poseidon/close_account_program/ts-programs/src/close_account_program.ts
+++ basics/close-account/poseidon/close_account_program/ts-programs/src/close_account_program.ts
@@ -1,10 +1,10 @@
-import { Account, String as PoseidonString, Pubkey, Result, Signer, u8 } from '@solanaturbine/poseidon';
+import { Account, String, Pubkey, Result, Signer, u8 } from '@solanaturbine/poseidon';
 
 export default class CloseAccount {
   static PROGRAM_ID = new Pubkey('AtUc6zMfozxrQoK4PbDUnd5daS86XCPuT2og1293P5XXo');
 
-  createUser(user: Signer, userState: UserState, name: PoseidonString<50>): Result {
-    userState.derive(['USER', user.key]).init();
+  createUser(user: Signer, userState: UserState, name: String<50>): Result {
+    userState.derive(['USER', user.key]).init(user);
 
     userState.userBump = userState.getBump();
 
@@ -19,6 +19,6 @@ export default class CloseAccount {
 
 export interface UserState extends Account {
   userBump: u8;
-  name: PoseidonString<50>;
+  name: String<50>;
   user: Pubkey;
 }
