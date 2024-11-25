75bff6c367ff8182f543134822a17122aaf58bec
Memewtoo <myuutuh@gmail.com>
2024-11-25 11:55:09 +0800

    chore: format fixes

--- basics/counter/poseidon/tests/counter.ts
+++ basics/counter/poseidon/tests/counter.ts
@@ -1,10 +1,10 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Counter } from "../target/types/counter";
-import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-import { assert } from "chai";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+import { assert } from 'chai';
+import { Counter } from '../target/types/counter';
 
-describe("counter", () => {
+describe('counter', () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -21,10 +21,7 @@ describe("counter", () => {
     const latestBlockHash = await provider.connection.getLatestBlockhash();
 
     // Airdrop 1 SOL to the user
-    const airdropUser = await provider.connection.requestAirdrop(
-      user.publicKey,
-      1 * LAMPORTS_PER_SOL
-    );
+    const airdropUser = await provider.connection.requestAirdrop(user.publicKey, 1 * LAMPORTS_PER_SOL);
     await provider.connection.confirmTransaction({
       blockhash: latestBlockHash.blockhash,
       lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
@@ -32,72 +29,60 @@ describe("counter", () => {
     });
 
     // Derive PDA for the counter account
-    [counterPDA, counterBump] = PublicKey.findProgramAddressSync(
-      [Buffer.from("counter")],
-      program.programId
-    );
+    [counterPDA, counterBump] = PublicKey.findProgramAddressSync([Buffer.from('counter')], program.programId);
   });
 
-  it("Initializes a counter account", async () => {
-    
+  it('Initializes a counter account', async () => {
     // Invoke the Initialize Counter instruction from the program
     await program.methods
       .initializeCounter()
       .accountsPartial({
         payer: user.publicKey,
-        counter: counterPDA
+        counter: counterPDA,
       })
       .signers([user])
-      .rpc()
+      .rpc();
 
     // Fetch the counter account info
     const currentCounter = await program.account.counterAccount.fetch(counterPDA);
-    
+
     // Assert and compare the account data
-    assert.equal(currentCounter.count.toNumber(), 0, "Expected count to be 0");
+    assert.equal(currentCounter.count.toNumber(), 0, 'Expected count to be 0');
   });
 
-  it("Increments the counter account", async () => {
-    
+  it('Increments the counter account', async () => {
     // Invoke the Increment Counter instruction from the program
     await program.methods
       .incrementCounter()
       .accountsPartial({
         payer: user.publicKey,
-        counter: counterPDA
+        counter: counterPDA,
       })
       .signers([user])
-      .rpc()
-    
+      .rpc();
+
     // Fetch the counter account info
     const currentCounter = await program.account.counterAccount.fetch(counterPDA);
-    
+
     // Assert and compare the account data
-    assert.equal(currentCounter.count.toNumber(), 1, "Expected count to be 1");
+    assert.equal(currentCounter.count.toNumber(), 1, 'Expected count to be 1');
   });
 
-  it("Increments the counter account again", async () => {
-    
+  it('Increments the counter account again', async () => {
     // Invoke the Increment Counter instruction from the program
     await program.methods
       .incrementCounter()
       .accountsPartial({
         payer: user.publicKey,
-        counter: counterPDA
+        counter: counterPDA,
       })
       .signers([user])
-      .rpc()
-    
+      .rpc();
+
     // Fetch the counter account info
     const currentCounter = await program.account.counterAccount.fetch(counterPDA);
-    
+
     // Assert and compare the account data
-    assert.equal(currentCounter.count.toNumber(), 2, "Expected count to be 2");
+    assert.equal(currentCounter.count.toNumber(), 2, 'Expected count to be 2');
   });
 });
-
-
-
-  
-
- 
