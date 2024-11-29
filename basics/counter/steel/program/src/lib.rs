e48edb3da383b896e92703fd69e2fa92e8bba229
aryan <agarwalaryan139@gmail.com>
2024-11-29 15:30:05 +0530

    fix: warnings

--- basics/counter/steel/program/src/lib.rs
+++ basics/counter/steel/program/src/lib.rs
@@ -7,14 +7,12 @@ use initialize::*;
 use steel::*;
 use steel_api::prelude::*;
 
-use solana_program::msg;
-
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     data: &[u8],
 ) -> ProgramResult {
-    let (ix, data) = parse_instruction(&steel_api::ID, program_id, data)?;
+    let (ix, _data) = parse_instruction(&steel_api::ID, program_id, data)?;
 
     match ix {
         SteelInstruction::Initialize => process_initialize(accounts)?,
