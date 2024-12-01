aef66cf641952065c6ddeee079c2452f9d0e6aba
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-12-01 06:13:09 +0000

    refactored the code with new Poseidon Str naming type

--- basics/account-data/poseidon/account-data-program/ts-programs/src/accountDataProgram.ts
+++ basics/account-data/poseidon/account-data-program/ts-programs/src/accountDataProgram.ts
@@ -1,10 +1,10 @@
-import { Account, Pubkey, Result, Signer, u8, u16, u32 } from '@solanaturbine/poseidon';
+import { Account, Pubkey, Result, Signer, Str, u8, u16, u32 } from '@solanaturbine/poseidon';
 
 export default class AccountDataProgram {
   // Following Poseidon example pattern of static PROGRAM_ID
   static PROGRAM_ID = new Pubkey('JvF1QDhgab1ARhACPWTAZnUymthGGmn3NXCj8i6mjSQ');
 
-  create_address_info(payer: Signer, state: AddressInfo, name: String, houseNumber: u8, street: String, city: String): Result {
+  create_address_info(payer: Signer, state: AddressInfo, name: Str<25>, houseNumber: u8, street: Str<25>, city: Str<25>): Result {
     // Use nit() for initialization
     state.derive(['address_info', payer.key]).init(payer);
     // Store the account data
@@ -16,8 +16,8 @@ export default class AccountDataProgram {
 }
 
 export interface AddressInfo extends Account {
-  name: String;
+  name: Str<25>;
   houseNumber: u8;
-  street: String;
-  city: String;
+  street: Str<25>;
+  city: Str<25>;
 }
