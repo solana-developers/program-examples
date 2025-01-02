cb4b9696eeb9f4b2427900f0f9a88834813f8dc8
Eric Alaribe <42889673+funkycadet@users.noreply.github.com>
2025-01-02 14:51:52 +0100

    add tokens/create-token/steel (#299)

---
+++ tokens/create-token/steel/program/src/token.rs
@@ -0,0 +1,35 @@
+use solana_program::{msg, program_pack::Pack};
+use spl_token::state::Mint;
+use steel::*;
+use steel_api::prelude::*;
+
+pub fn process_create_token(
+    accounts: &[AccountInfo],
+    name: String,
+    symbol: String,
+) -> ProgramResult {
+    let mint = Mint {
+        mint_authority: COption::Some(accounts[0].key.to_owned()),
+        supply: 0,
+        decimals: 2,
+        is_initialized: false,
+        freeze_authority: COption::None,
+    };
+
+    let mut data = vec![];
+    mint.pack_into_slice(&mut data);
+
+    let mut instruction = Instruction::new(
+        accounts[0].key.to_owned(),
+        vec![],
+        vec![AccountMeta::new_readonly(accounts[0].key.to_owned(), false)],
+        data,
+    );
+
+    let mut instruction_data = vec![];
+    instruction.pack_into_slice(&mut instruction_data);
+
+    msg!("Instruction data: {:?}", instruction_data);
+
+    Ok(())
+}
