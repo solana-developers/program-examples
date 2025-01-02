136b6f2682eeafcf3aa3323b4db16dc3c1367128
Mewtoo <60632424+Memewtoo@users.noreply.github.com>
2025-01-02 21:51:09 +0800

    add basics/transfer-sol/poseidon (#243)
    Co-authored-by: Ayush <ayush-15@live.com>

---
+++ basics/transfer-sol/poseidon/programs/transfer-sol/src/lib.rs
@@ -0,0 +1,30 @@
+use anchor_lang::prelude::*;
+use anchor_lang::system_program::{transfer, Transfer};
+declare_id!("BLiyCbPDx54vqpNPQG6A7YAqEM1vRHiFfvReMKC4FFk5");
+#[program]
+pub mod transfer_sol {
+    use super::*;
+    pub fn transfer_sol_with_cpi(
+        ctx: Context<TransferSolWithCpiContext>,
+        amount: u64,
+    ) -> Result<()> {
+        let transfer_accounts = Transfer {
+            from: ctx.accounts.payer.to_account_info(),
+            to: ctx.accounts.recipient.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            transfer_accounts,
+        );
+        transfer(cpi_ctx, amount)?;
+        Ok(())
+    }
+}
+#[derive(Accounts)]
+pub struct TransferSolWithCpiContext<'info> {
+    #[account(mut)]
+    pub recipient: SystemAccount<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
