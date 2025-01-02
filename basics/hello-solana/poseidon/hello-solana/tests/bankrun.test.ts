82a68b32d517b2e08910677eb4ff9d0bf0ffe037
Onyewuchi Emmanuel <marrnuel123@gmail.com>
2025-01-02 14:34:49 +0100

    add basics/hello-solana/poseidon (#139)

---
+++ basics/hello-solana/poseidon/hello-solana/tests/bankrun.test.ts
@@ -0,0 +1,32 @@
+import * as anchor from "@coral-xyz/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { BankrunProvider } from "anchor-bankrun";
+import { assert } from "chai";
+import { startAnchor } from "solana-bankrun";
+import { HelloSolanaProgram } from "../target/types/hello_solana_program"; // Assuming this path
+
+const IDL = require("../target/idl/hello_solana_program.json");
+const PROGRAM_ID = new PublicKey(IDL.metadata.address);
+
+describe("hello_solana_program (Bankrun)", async () => {
+  const context = await startAnchor(
+    "",
+    [{ name: "hello_solana_program", programId: PROGRAM_ID }],
+    []
+  );
+
+  const provider = new BankrunProvider(context);
+
+  const program = new anchor.Program<HelloSolanaProgram>(
+    IDL,
+    PROGRAM_ID,
+    provider
+  );
+
+  it("Executes 'hello' successfully", async () => {
+    const tx = await program.methods.hello().rpc();
+
+    // Chai assert to ensure no error occurred and transaction completed successfully
+    assert.isOk(tx, "Transaction should complete without errors");
+  });
+});
