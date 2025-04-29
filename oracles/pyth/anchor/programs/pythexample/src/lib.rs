c354e8556dba02c1e5dc80c2817063e9c0f15b72
Aaron Bassett <aaronbassett@gmail.com>
2025-04-29 14:46:56 +0100

    chore: Update Pyth Oracle Example Program (#375)
    * chore: remove non-functioning Pyth Oracle Anchor program
    
    The SDK version used in the example is no longer supported and
    relies on the legacy Push Oracle, which has been decommissioned.
    
    Removing it to ensure the examples remain accurate and functional.
    
    * feat: add new Pyth Oracle example
    
    * chore: update program name and meta information
    
    The original Pyth Oracle example program has backlinks from external
    sites. Rename the new example program to `pythexample` so the
    directory structure and GitHub URL remain the same so we hopefully
    don't break links on external sites.
    
    * chore: migrate from Yarn to pnpm as per contributing guide
    
    * chore: remove unused files and configs
    
    Project no longer uses the Anchor client or any other TypeScript
    code/packages. Clean up related obsolete files.

--- oracles/pyth/anchor/programs/pythexample/src/lib.rs
+++ oracles/pyth/anchor/programs/pythexample/src/lib.rs
@@ -1,45 +1,24 @@
-#![allow(clippy::result_large_err)]
-
+use pyth_solana_receiver_sdk::price_update::PriceUpdateV2;
 use anchor_lang::prelude::*;
-pub mod state;
-use state::PriceFeed;
 
-pub mod error;
-use error::ErrorCode;
-
-declare_id!("F6mNuN1xoPdRaZcUX3Xviq7x1EFtoBXygpFggCLd62eU");
+declare_id!("GUkjQmrLPFXXNK1bFLKt8XQi6g3TjxcHVspbjDoHvMG2");
 
 #[program]
-pub mod pythexample {
+pub mod anchor_test {
     use super::*;
-    pub fn read_price(ctx: Context<Pyth>) -> Result<()> {
-        let price_feed = &ctx.accounts.price_feed;
-        let clock = &ctx.accounts.clock;
-        // Get the current timestamp
-        let timestamp: i64 = clock.unix_timestamp;
-        // Load the price from the price feed. Here, the price can be no older than 500 seconds.
-        let price: pyth_sdk::Price = price_feed
-            .get_price_no_older_than(timestamp, 30)
-            .ok_or(ErrorCode::PythError)?;
 
-        let confidence_interval: u64 = price.conf;
-
-        let asset_price_full: i64 = price.price;
-
-        let asset_exponent: i32 = price.expo;
-
-        let asset_price = asset_price_full as f64 * 10f64.powi(asset_exponent);
-
-        msg!("Price: {}", asset_price);
-        msg!("Confidence interval: {}", confidence_interval);
-
+    pub fn read_price(ctx: Context<ReadPrice>) -> Result<()> {
+        let price_update = &ctx.accounts.price_update;
+        msg!("Price feed id: {:?}", price_update.price_message.feed_id);
+        msg!("Price: {:?}", price_update.price_message.price);
+        msg!("Confidence: {:?}", price_update.price_message.conf);
+        msg!("Exponent: {:?}", price_update.price_message.exponent);
+        msg!("Publish Time: {:?}", price_update.price_message.publish_time);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Pyth<'info> {
-    pub price_feed: Account<'info, PriceFeed>,
-    pub system_program: Program<'info, System>,
-    pub clock: Sysvar<'info, Clock>,
+pub struct ReadPrice<'info> {
+    pub price_update: Account<'info, PriceUpdateV2>,
 }
