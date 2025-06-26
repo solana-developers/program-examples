82e276a2bb88e85d5c8aa75d1488584934a2b842
ubadineke <ubadinekeprince@gmail.com>
2025-06-26 09:51:10 +0100

    correct naming

--- tokens/token-2022/mint-close-authority/steel/program/src/mint_close.rs
+++ tokens/token-2022/mint-close-authority/steel/program/src/mint_close.rs
@@ -4,13 +4,13 @@ use steel::*;
 
 pub fn process_mint_close(accounts: &[AccountInfo<'_>], data: &[u8]) -> ProgramResult {
     // Load accounts.
-    let [signer_info, mint_info, destination, system_program, token_program] = accounts else {
+    let [signer_info, mint_info, destination_info, system_program, token_program] = accounts else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
     signer_info.is_signer()?;
     mint_info.is_signer()?;
-    destination.is_writable()?;
+    destination_info.is_writable()?;
     token_program.is_program(&spl_token_2022::ID)?;
     system_program.is_program(&system_program::ID)?;
 
@@ -18,11 +18,11 @@ pub fn process_mint_close(accounts: &[AccountInfo<'_>], data: &[u8]) -> ProgramR
         &close_account(
             token_program.key,
             mint_info.key,
-            destination.key,
+            destination_info.key,
             signer_info.key,
             &[],
         )?,
-        &[mint_info.clone(), destination.clone(), signer_info.clone()],
+        &[mint_info.clone(), destination_info.clone(), signer_info.clone()],
     )?;
 
     msg!("Mint Close Authority Extension: Account Closed!");
--- tokens/token-2022/transfer-fee/steel/program/src/harvest.rs
+++ tokens/token-2022/transfer-fee/steel/program/src/harvest.rs
@@ -4,7 +4,7 @@ use steel::*;
 
 pub fn process_harvest(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramResult {
     //Load accounts
-    let [signer_info, mint_info, harvest_acc_1, harvest_acc_2, system_program, token_program] =
+    let [signer_info, mint_info, harvest_acc_1_info, harvest_acc_2_info, system_program, token_program] =
         accounts
     else {
         return Err(ProgramError::NotEnoughAccountKeys);
@@ -14,19 +14,19 @@ pub fn process_harvest(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramRes
     mint_info.is_writable()?;
     token_program.is_program(&spl_token_2022::ID)?;
     system_program.is_program(&system_program::ID)?;
-    harvest_acc_1.is_writable()?;
-    harvest_acc_2.is_writable()?;
+    harvest_acc_1_info.is_writable()?;
+    harvest_acc_2_info.is_writable()?;
 
     invoke(
         &harvest_withheld_tokens_to_mint(
             token_program.key,
             mint_info.key,
-            &[&harvest_acc_1.key, &harvest_acc_2.key],
+            &[&harvest_acc_1_info.key, &harvest_acc_2_info.key],
         )?,
         &[
             mint_info.clone(),
-            harvest_acc_1.clone(),
-            harvest_acc_2.clone(),
+            harvest_acc_1_info.clone(),
+            harvest_acc_2_info.clone(),
         ],
     )?;
 
