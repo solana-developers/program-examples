c17918f7884b0384aa520ef441f468696c430f81
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2025-10-01 03:40:24 +0100

    add allow unexpected cfgs and update deps in basic examples

--- basics/checking-accounts/native/program/src/lib.rs
+++ basics/checking-accounts/native/program/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unexpected_cfgs)]
+
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
--- basics/rent/native/program/src/lib.rs
+++ basics/rent/native/program/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unexpected_cfgs)]
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
