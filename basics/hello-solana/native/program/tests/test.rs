7a49e9489addb36f5b1d4bbea1e9457661b09ed9
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2025-11-19 06:34:18 +0100

    add rust test for hello solana (#443)
    * add rust test for hello solana
    
    * Update test.rs
    
    * Update test.rs

---
+++ basics/hello-solana/native/program/tests/test.rs
@@ -0,0 +1,34 @@
+use litesvm::LiteSVM;
+use solana_instruction::{AccountMeta, Instruction};
+use solana_keypair::{Keypair, Signer};
+use solana_native_token::LAMPORTS_PER_SOL;
+use solana_pubkey::Pubkey;
+use solana_transaction::Transaction;
+
+#[test]
+fn test_hello_solana() {
+    let program_id = Pubkey::new_unique();
+    let program_bytes = include_bytes!("../../tests/fixtures/hello_solana_program.so");
+
+    let mut svm = LiteSVM::new();
+    svm.add_program(program_id, program_bytes).unwrap();
+
+    let payer = Keypair::new();
+
+    svm.airdrop(&payer.pubkey(), LAMPORTS_PER_SOL * 10).unwrap();
+
+    let ix = Instruction {
+        program_id,
+        accounts: vec![AccountMeta::new(payer.pubkey(), true)],
+        data: vec![0],
+    };
+
+    let tx = Transaction::new_signed_with_payer(
+        &[ix],
+        Some(&payer.pubkey()),
+        &[&payer],
+        svm.latest_blockhash(),
+    );
+
+    assert!(svm.send_transaction(tx).is_ok());
+}
