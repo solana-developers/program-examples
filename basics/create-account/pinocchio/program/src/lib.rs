46031becb97e9761c6cc1d6e98631d6464b9c7c5
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2025-11-27 19:40:06 +0100

    add create account pinocchio example

---
+++ basics/create-account/pinocchio/program/src/lib.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![allow(deprecated)]
+
+use pinocchio::{
+    account_info::AccountInfo, entrypoint, nostd_panic_handler, program_error::ProgramError,
+    pubkey::Pubkey, ProgramResult,
+};
+use pinocchio_log::log;
+
+use pinocchio_system::instructions::CreateAccount;
+
+entrypoint!(process_instruction);
+nostd_panic_handler!();
+
+const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let [payer, new_account, _system_program] = accounts else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    log!("Program invoked. Creating a system account...");
+    log!("  New public key will be:");
+    pinocchio::pubkey::log(&new_account.key());
+
+    CreateAccount {
+        from: payer,
+        to: new_account,
+        lamports: LAMPORTS_PER_SOL,
+        space: 0,
+        owner: &pinocchio_system::ID,
+    }
+    .invoke()?;
+
+    log!("Account created succesfully.");
+    Ok(())
+}
---
+++ basics/create-account/pinocchio/tests/test.ts
@@ -0,0 +1,63 @@
+import { describe, test } from "node:test";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import { start } from "solana-bankrun";
+
+describe("Create a system account", async () => {
+  const PROGRAM_ID = PublicKey.unique();
+  const context = await start(
+    [{ name: "create_account_pinocchio_program", programId: PROGRAM_ID }],
+    [],
+  );
+  const client = context.banksClient;
+  const payer = context.payer;
+
+  test("Create the account via a cross program invocation", async () => {
+    const newKeypair = Keypair.generate();
+    const blockhash = context.lastBlockhash;
+
+    const ix = new TransactionInstruction({
+      keys: [
+        { pubkey: payer.publicKey, isSigner: true, isWritable: true },
+        { pubkey: newKeypair.publicKey, isSigner: true, isWritable: true },
+        { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
+      ],
+      programId: PROGRAM_ID,
+      data: Buffer.alloc(0),
+    });
+
+    const tx = new Transaction();
+    tx.recentBlockhash = blockhash;
+    tx.add(ix).sign(payer, newKeypair);
+
+    await client.processTransaction(tx);
+  });
+
+  test("Create the account via direct call to system program", async () => {
+    const newKeypair = Keypair.generate();
+    const blockhash = context.lastBlockhash;
+
+    const ix = SystemProgram.createAccount({
+      fromPubkey: payer.publicKey,
+      newAccountPubkey: newKeypair.publicKey,
+      lamports: LAMPORTS_PER_SOL,
+      space: 0,
+      programId: SystemProgram.programId,
+    });
+
+    const tx = new Transaction();
+    tx.recentBlockhash = blockhash;
+    tx.add(ix).sign(payer, newKeypair);
+
+    await client.processTransaction(tx);
+    console.log(
+      `Account with public key ${newKeypair.publicKey} successfully created`,
+    );
+  });
+});
