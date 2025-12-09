4c7f36b519dff7964500034664eed6298fd74e69
solwarrior <118261237+sol-warrior@users.noreply.github.com>
2025-12-09 08:16:02 +0530

    refactor: update hello-solana Anchor test file to use type-only imports and clearer suite descriptions

--- basics/hello-solana/anchor/tests/test.ts
+++ basics/hello-solana/anchor/tests/test.ts
@@ -1,17 +1,17 @@
-import * as anchor from '@coral-xyz/anchor';
-import { HelloSolana } from '../target/types/hello_solana';
+import * as anchor from "@coral-xyz/anchor";
+import type { HelloSolana } from "../target/types/hello_solana.ts";
 
-describe('hello-solana', () => {
-  // Configure the Anchor provider & load the program IDL
-  // The IDL gives you a typescript module
-  //
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
-  const program = anchor.workspace.HelloSolana as anchor.Program<HelloSolana>;
+describe("Anchor: hello-solana", () => {
+	// Configure the Anchor provider & load the program IDL
+	// The IDL gives you a typescript module
+	//
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+	const program = anchor.workspace.HelloSolana as anchor.Program<HelloSolana>;
 
-  it('Say hello!', async () => {
-    // Just run Anchor's IDL method to build a transaction!
-    //
-    await program.methods.hello().accounts({}).rpc();
-  });
+	it("Say hello!", async () => {
+		// Just run Anchor's IDL method to build a transaction!
+		//
+		await program.methods.hello().accounts({}).rpc();
+	});
 });
