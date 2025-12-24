d42f0d7f5875adce068a090012d434adc92c8008
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2025-12-24 18:08:10 +0100

    add proccessing ix pinocchio example

---
+++ basics/processing-instructions/pinocchio/tests/test.ts
@@ -0,0 +1,13 @@
+import { Buffer } from "node:buffer";
+import { describe, test } from "node:test";
+import {
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import * as borsh from "borsh";
+import { start } from "solana-bankrun";
+
+describe("custom-instruction-data", async () => {
+  console.log("custom-instruction-data");
+});
